b6f471d5770961f84c4f5ea12f3f660a97afa6d6
Samrudh Sivva <samrudh5000@gmail.com>
2022-07-08 08:27:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express, { json } from 'express';
-import { router } from './src/routes/router.js';
+import express, { json } from "express";
+import {router} from "./src/routes/router.js";
 
-const app = express();
-const PORT_NUMBER = process.env.PORT || 3000;
+const app=express();
+
 app.use(json());
-app.use('/', router);
+app.use("/",router);
 
-app.listen(PORT_NUMBER, () => {
-    console.log(`Server is running on port ${PORT_NUMBER}`, `visit http://localhost:${PORT_NUMBER}`);
-});
+app.listen(3000,()=>{
+    console.log("Server is listening to PORT 3000");
+})
